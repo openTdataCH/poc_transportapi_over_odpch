22c834148dddca38328f6d5dd7765cbb4616da70
Maki Mikkelson <info@maki3000.net>
2023-04-19 19:23:38 +0200

    Format slightly

--- public_html/buffer.php
+++ public_html/buffer.php
@@ -1,19 +1,18 @@
 <?php 
-
 /*
-Copyright 2016 Matthias Günter, GnostX GmbH
+    Copyright 2016 Matthias Günter, GnostX GmbH
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 
 //As we are session based, we don't need a buffer that expires
@@ -25,13 +24,11 @@ Checks if the request is already in the buffer
 * @param $query - the query asked
 * @returns true, when the buffer contains the value
 */
-function inbuffer($fct,$query){
+function inbuffer($fct, $query) {
 	global $bigbuffer;	
-	if (isset ($bigbuffer[$fct."##".$query])){
-		//file_put_contents("php://stdout", "\nbuffer hit: $fct\n");
+	if (isset($bigbuffer[$fct . "##" . $query])){
 		return true;
 	}
-	//file_put_contents("php://stdout", "\nbuffer failed: $fct\n");
 	return false;
 }
 
@@ -41,12 +38,9 @@ Obtains the value from the buffer
 * @param $query - the query asked
 * @returns the buffer value or null
 */
-function getbuffer($fct,$query){
-	
-	global $bigbuffer;
-	//file_put_contents("php://stdout", "\nused buffer: $fct\n");
-	
-	return $bigbuffer[$fct."##".$query];
+function getbuffer($fct, $query) {
+	global $bigbuffer;	
+	return $bigbuffer[$fct . "##" . $query];
 }
 
 /*
@@ -55,11 +49,9 @@ Sets the value in the buffer
 * @param $query - the query asked
 * @returns no return value
 */
-
-function setbuffer($fct,$query,$val){
+function setbuffer($fct, $query, $val) {
 	global $bigbuffer;
-	//file_put_contents("php://stdout", "\nbuffering $fct: $query\n");
-	$bigbuffer[$fct."##".$query]=$val;
-	
+	$bigbuffer[$fct . "##" . $query] = $val;	
 }
+
 ?>
